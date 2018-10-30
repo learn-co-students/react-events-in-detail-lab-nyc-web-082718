35d1feda658c73fb8c48b4ffdb9529a53b03ef45
mguevara717 <marissa.guevara717@gmail.com>
2018-10-30 17:42:18 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react'
+
+class CoordinatesButton extends Component {
+//if the class is not going to receive props, then you dont need a constructor.
+//when you dont need to set state inside of the component, then you dont need a constructor.
+
+  handleClick = (event) => {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY])
+    //accessing the props passed to it from index.js
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>Coordinate Button</button>
+    )
+  } //invoking class method on line 15
+}
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react'
+
+class DelayedButton extends Component {
+
+  handleClick = (event) => {
+    event.persist(); //makes the event last long enough to call it back
+    setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+  } //the naming convention for handling clicks OR events, should always be 'handle[followed by name of the event]'
+
+
+  render() {
+    return (
+    <button onClick={this.handleClick}>Delayed Button</button>
+    )
+  }
+
+}//end of class
+
+export default DelayedButton
